aa04dfbbfb9a355466aecf6dbeb98d5dee2e6d0b
Veselin <vesselin60@gmail.com>
2020-12-15 15:04:31 +0200

    353. ListView in Practice

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import { FlatList } from 'react-native';
 import { connect } from 'react-redux';
 
 class LibraryList extends Component {
+  renderItem(library) {
+
+  }
+
   render() {
-    console.log(this.props);
-    return undefined;
+    return (
+      <FlatList
+        data={this.props.libraries}
+        renderItem={this.renderItem}
+        keyExtractor={(library) => library.id}
+      />
+    );
   }
 }
 
