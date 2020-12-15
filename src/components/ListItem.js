5a1ff3e9a6f75fd1b62010eecf75d62e479e44dc
Veselin <vesselin60@gmail.com>
2020-12-15 17:51:02 +0200

    358. Calling Action Creators

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { Text, StyleSheet, View } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
   render() {
+    console.log(this.props);
     return (
       <CardSection>
         <Text style={styles.titleStyle}>{this.props.library.item.title}</Text>
@@ -19,4 +22,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default ListItem;
+export default connect(null, actions)(ListItem);
