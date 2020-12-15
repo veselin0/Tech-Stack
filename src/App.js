227c6c999940babb86c8374e0ad99ba32737af4c
Veselin <vesselin60@gmail.com>
2020-12-15 10:24:46 +0200

    344. More on Redux Boilerplate

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 
 const App = () => {
-  return <View />;
+  return (
+    <Provider store={createStore(reducers)}>
+      <View />
+    </Provider>
+  );
 };
 
 export default App;
