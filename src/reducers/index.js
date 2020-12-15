d413ffe074ed6d80b0c981cea64c1fc40a77ca80
Veselin <vesselin60@gmail.com>
2020-12-15 14:28:02 +0200

    349. The Connect Function

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,5 +2,5 @@ import { combineReducers } from 'redux';
 import LibraryReducer from './LibraryReducer';
 
 export default combineReducers({
-  libraies: LibraryReducer,
+  libraries: LibraryReducer,
 });
